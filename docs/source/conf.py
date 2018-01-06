88d88f10062822f8298cc7c4b53878e5da52457e
Vladimir Botka <vbotka@gmail.com>
2018-01-06 10:35:09 +0100

    Fixed html_theme_path.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,6 +91,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 else:
     # html_theme = 'default'
     html_theme = "guzzle_sphinx_theme"
+    html_theme_path = ['_themes', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
