ad70af4ca115dc40600291ee2d645e438b6e0634
Vladimir Botka <vbotka@gmail.com>
2019-09-17 12:22:06 +0200

    Theme changed to sphinx_rtd_theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,8 +84,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     #    import sphinx_rtd_theme
     # html_theme = 'alabaster'
     # html_theme = 'default'
-    html_theme = "guzzle_sphinx_theme"
-    # html_theme = "sphinx_rtd_theme"
+    # html_theme = "guzzle_sphinx_theme"
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = ['_themes', ]
     # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
     #
@@ -95,8 +95,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     # html_static_path = ['_themes', ]
 else:
     # html_theme = 'default'
-    html_theme = "guzzle_sphinx_theme"
-    # html_theme = "sphinx_rtd_theme"
+    # html_theme = "guzzle_sphinx_theme"
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = ['_themes', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
